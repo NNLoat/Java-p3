75ef8a87788d98ac6101922dd114b648fe936b75
Earthworld <Earthworld@MSI>
2021-04-24 11:46:30 +0700

    test cooment

--- test2/src/Member.java
+++ test2/src/Member.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 public class Member {
+	//fuck u
 	protected String email;
 	protected String password;
 	protected ArrayList<Video> videoList = new ArrayList<Video>();
